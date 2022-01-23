b2c606245134042cd0a6fc44616be9c660e4f0a3
冥天肝 <97952402+WeZhengTing@users.noreply.github.com>
2022-01-23 11:10:23 +0800

    Update signIzhichen.py
    在izhichensign.py第30行才是改你的学号与名字

--- signIzhichen.py
+++ signIzhichen.py
@@ -2,8 +2,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from  Izhichen import izhichen
-def run_izhichen():
-    izhichen()
 
 class Mail:
     def __init__(self):
@@ -11,15 +9,15 @@ class Mail:
 
         self.mail_host="smtp.qq.com"       #设置服务器:这个是qq邮箱服务器，直接复制就可以
         self.mail_pass="spuhpmjvmikjdgij"           #刚才我们获取的授权码
-        self.sender = '2946073318@qq.com'      #你的邮箱地址
-        self.receivers = ['2946073318@qq.com']  # 收件人的邮箱地址
+        self.sender = '@qq.com'      #你的邮箱地址
+        self.receivers = ['@qq.com']  # 收件人的邮箱地址
 
     def send(self):
 
         content = '签到成功啦'
         message = MIMEText(content, 'plain', 'utf-8')
 
-        message['From'] = Header("自己哦", 'utf-8')
+        message['From'] = Header("健康日报", 'utf-8')
         message['To'] = Header("自己", 'utf-8')
 
         subject = '日报通知'  # 发送的主题，可自由填写
@@ -29,7 +27,7 @@ class Mail:
             smtpObj.login(self.sender, self.mail_pass)
             smtpObj.sendmail(self.sender, self.receivers, message.as_string())
             smtpObj.quit()
-            run_izhichen()
+            izhichen('211816452','李白')
             print("OK滚去睡觉吧")
         except smtplib.SMTPException as e:
             print("快自己起床签到")
@@ -50,7 +48,6 @@ class Mail:
             smtpObj.login(self.sender, self.mail_pass)
             smtpObj.sendmail(self.sender, self.receivers, message.as_string())
             smtpObj.quit()
-            run_izhichen()
             print("OK滚去睡觉吧")
         except smtplib.SMTPException as e:
             print("快自己起床签到")
