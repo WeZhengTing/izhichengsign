e79039bba70f71c453501478f852d501f611c2f7
冥天肝 <97952402+WeZhengTing@users.noreply.github.com>
2022-01-24 15:54:18 +0800

    Create yun_sign.py

---
+++ yun_sign.py
@@ -0,0 +1,64 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from time import strftime,gmtime
+import datetime
+class Mail:
+    def __init__(self):
+        # 第三方 SMTP 服务
+
+        self.mail_host="smtp.qq.com"       #设置服务器:这个是qq邮箱服务器，直接复制就可以
+        self.mail_pass="spuhpmjvmikjdgij"           #刚才我们获取的授权码
+        self.sender = '@qq.com'      #你的邮箱地址
+        self.receivers = ['@qq.com']  # 收件人的邮箱地址
+
+    def send(self):
+        times=(datetime.datetime.today()+datetime.timedelta(hours=8)).strftime("%Y-%m-%d_%H:%M")
+        content = '签到成功啦,'+times
+        message = MIMEText(content, 'plain', 'utf-8')
+
+        message['From'] = Header("健康日报", 'utf-8')
+        message['To'] = Header("自己", 'utf-8')
+
+        subject = '日报通知'  # 发送的主题，可自由填写
+        message['Subject'] = Header(subject, 'utf-8')
+        try:
+            smtpObj = smtplib.SMTP_SSL(self.mail_host, 465)
+            smtpObj.login(self.sender, self.mail_pass)
+            smtpObj.sendmail(self.sender, self.receivers, message.as_string())
+            smtpObj.quit()
+        except smtplib.SMTPException as e:
+            print("快自己起床签到")
+            print(e)
+
+
+    def sendfalse(self):
+
+        content = '签到失败啦'
+        message = MIMEText(content, 'plain', 'utf-8')
+
+        message['From'] = Header("健康日报", 'utf-8')
+        message['To'] = Header("自己", 'utf-8')
+
+        subject = '日报通知'  # 发送的主题，可自由填写
+        message['Subject'] = Header(subject, 'utf-8')
+        try:
+            smtpObj = smtplib.SMTP_SSL(self.mail_host, 465)
+            smtpObj.login(self.sender, self.mail_pass)
+            smtpObj.sendmail(self.sender, self.receivers, message.as_string())
+            smtpObj.quit()
+        except smtplib.SMTPException as e:
+            print("快自己起床签到")
+            print(e)
+
+
+
+def sign():
+    mail = Mail()
+
+    try:
+        mail.send()
+
+    except:
+        mail.sendfalse()
+        #打包pyinstaller -F -w signIzhichen.py
